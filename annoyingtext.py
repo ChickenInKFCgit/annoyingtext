eed2b2578889a6615062074d093b58f37a48ce51
Ewen <linktriforce2611@gmail.com>
2026-06-22 18:27:25 +0200

    Chargement des données dans un fichier tsv, amélioration du code (lisibilité & efficacité) et ajout de fonction de sauvegarde et de chargements des lettres

--- annoyingtext.py
+++ annoyingtext.py
@@ -10,13 +10,40 @@ Lettres={'A':['A','Ẳ','ᾏ'],'B':['Ḇ','β','₿','ẞ','ϐ'],'C':['©','Ç',
 '6':['₆','⁶','6','6','6','6','6','6'],'7':['⁷','₇','⅞','7','7','7','7','7'],'8':['⁸','₈','8','8','8','8','8','8'],'9':['₉','9','9','9','9','9','9','9']}
 
 
-def codertexte(texte,d):
-	texte,st=list(texte),''
-	for k in range(len(texte)):
-		if texte[k] in d.keys():
-			liste=d[texte[k]]
-			texte[k]=liste[randint(0,len(liste)-1)]
-	for y in texte:
-		st+=(y)
-	return st
+def codertexte(texte):
+	res = ""
+	for lettre in texte:
+		if lettre in Lettres:
+			possibilites = Lettres[lettre]
+			choisie = randint(0, len(possibilites)-1)
 
+			res+=possibilites[choisie]
+		else:
+			res+=lettre
+	return res
+
+
+def load_dict_lettres():
+	d = {}
+
+	f = open(PATH_LETTRES, mode="r",encoding=ENCODING)
+	for ligne in f.read().split("\n"):
+		possibilites = ligne.split("\t")
+		lettre = possibilites.pop(0)
+		d[lettre] = possibilites
+	
+	return d
+			
+
+
+def save_dict_lettres():
+	f = open(PATH_LETTRES, mode="w+",encoding=ENCODING)
+	for lettre in Lettres.keys():
+		ligne = f"\n{lettre}"
+		for variation in Lettres[lettre]:
+			ligne+=f"\t{variation}"
+		f.write(ligne)
+
+PATH_LETTRES = "lettres.tsv"
+ENCODING = "utf-16"
+Lettres = load_dict_lettres()
